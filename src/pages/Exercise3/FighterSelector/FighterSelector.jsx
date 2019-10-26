54fc50102cc188132630a3dc2c2609c7a134e24d
farzac <zaccantte@gmail.com>
2019-10-26 16:50:56 -0300

    Correction Challenge day

--- src/pages/Exercise3/FighterSelector/FighterSelector.jsx
+++ src/pages/Exercise3/FighterSelector/FighterSelector.jsx
@@ -4,43 +4,46 @@ import FighterSlider from "../FighterSlider/FighterSlider";
 import styles from "./FighterSelector.module.scss";
 
 function FighterSelector() {
-    const [fighter, setFighter] = useState("ken");
-    const [fighterSelectionHistory, setFighterSelectionHistory] = useState([]);
+  //const images =
+  const [fighter, setFighter] = useState("ken");
+  //const [fighter, setFighter] = useState([]);
+  const [fighterSelectionHistory, setFighterSelectionHistory] = useState([]);
 
-    const onFighterChange = fighter => {
-        setFighter(fighter);
-    }
+  const onFighterChange = fighter => {
+    alert(fighter);
+    setFighter(fighter);
+    //setFighter("ken");
+  };
 
-    useEffect(() => {
-        setTimeout(() => {
-            setFighterSelectionHistory([
-                ...fighterSelectionHistory,
-                {
-                    fighter,
-                    time: (new Date()).toLocaleTimeString()
-                }
-            ]);
-        }, 1000);
-    }, [fighter])
+  useEffect(() => {
+    setTimeout(() => {
+      setFighterSelectionHistory([
+        ...fighterSelectionHistory,
+        {
+          fighter,
+          time: new Date().toLocaleTimeString()
+        }
+      ]);
+      //}, 1000);
+    }, 1);
+  }, [fighter]);
 
-    return (
-        <div className={styles.FighterSelector}>
-            <div className={styles.FighterName}>{fighter}</div>
-            <FighterSlider
-                fighter={fighter}
-                onFighterChange={onFighterChange}>
-                <Fighter fighter={fighter} />
-            </FighterSlider>
-            <div className={styles.HistoryLog}>
-                <span>History:</span>
-                <div>
-                    {fighterSelectionHistory.map(({ fighter, time }) => (
-                        <p key={fighter+time}>{`[${time}] ${fighter} was selected.`}</p>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className={styles.FighterSelector}>
+      <div className={styles.FighterName}>{fighter}</div>
+      <FighterSlider fighter={fighter} onFighterChange={onFighterChange}>
+        <Fighter fighter={fighter} />
+      </FighterSlider>
+      <div className={styles.HistoryLog}>
+        <span>History:</span>
+        <div>
+          {fighterSelectionHistory.map(({ fighter, time }) => (
+            <p key={fighter + time}>{`[${time}] ${fighter} was selected.`}</p>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default FighterSelector;
